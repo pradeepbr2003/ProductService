54cb1173c89c44d5b17e1d5604836299561a24b4
Pradeep Ramaiah <pradeepbr2003@gmail.com>
2025-10-21 19:06:05 +0530

    Product Service full code added
    added central excep handling
    
    Update README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# Product Service
+
+## Please use swaager link to know about all endpoints 
+
+http://localhost:8080/swagger-ui/index.html
--- src/main/java/com/accenture/product/advice/ControllerAdvice.java
+++ src/main/java/com/accenture/product/advice/ControllerAdvice.java
@@ -1,10 +1,12 @@
 package com.accenture.product.advice;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Hidden
 @RestControllerAdvice
 public class ControllerAdvice {
 
